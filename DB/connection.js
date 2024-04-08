9a6142f330ef6fe906071e07ee3000a16b012f8c
mostafa khattab <mostafakhattab1555@gmail.com>
2024-04-08 21:15:38 +0200

    last v3
    lasttv3333

--- DB/connection.js
+++ DB/connection.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
 
-    return await mongoose.connect(process.env.DB_LOCAL)
-    .then(res=>console.log(`DB Connected successfully on .........`))
-    .catch(err=>console.log(` Fail to connect  DB.........${err} `))
+    return await mongoose.connect(process.env.DB_LOCAL || "mongodb+srv://couponesta:2002246810@cluster0.yfj3znp.mongodb.net/CouponestaDB")
+        .then(res => console.log(`DB Connected successfully on .........`))
+        .catch(err => console.log(` Fail to connect  DB.........${err} `))
 }
 
 

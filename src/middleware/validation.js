9b23ceaa9729a741f344c460d2c58ffde6e61539
mostafa khattab <mostafakhattab1555@gmail.com>
2024-04-08 11:50:07 +0200

    deploy v-1
    deploy version 1

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -2,8 +2,6 @@ import joi from 'joi'
 import { Types } from 'mongoose'
 
 const validateObjectId = (value, helper) => {
-    // console.log({ value });
-    // console.log(helper);
     return Types.ObjectId.isValid(value) ? true : helper.message('In-valid objectId')
 }
 
@@ -39,12 +37,6 @@ export const validation = (schema) => {
     return (req, res, next) => {
 
         let inputsData = { ...req.body, ...req.params, ...req.query }
-
-        // console.log(inputsData);
-        // if (req.file || req.files) {
-        //     inputsData.file = req.file || req.files
-
-        // }
 
         const validationResult = schema.validate(inputsData, { abortEarly: false })
         if (validationResult.error) {

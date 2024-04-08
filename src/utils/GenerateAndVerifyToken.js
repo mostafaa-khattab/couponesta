4ad8da8e849f5a168e7a8f60aa9146de86822ea1
mostafa khattab <mostafakhattab1555@gmail.com>
2024-04-08 21:59:14 +0200

    end
    end

--- src/utils/GenerateAndVerifyToken.js
+++ src/utils/GenerateAndVerifyToken.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken'
 
 
-export const generateToken = ({ payload = {}, signature = process.env.TOKEN_SIGNATURE, expiresIn = 60 * 60 } = {}) => {
+export const generateToken = ({ payload = {}, signature = `${process.env.EMAIL_TOKEN}` || "saving-coupons-signature by khattab@gmail.com", expiresIn = 60 * 60 } = {}) => {
     const token = jwt.sign(payload, signature, { expiresIn: parseInt(expiresIn) });
     return token
 }
 
-export const verifyToken = ({ token , signature = process.env.TOKEN_SIGNATURE } = {}) => {
+export const verifyToken = ({ token, signature = `${process.env.EMAIL_TOKEN}` || "saving-coupons-signature by khattab@gmail.com" } = {}) => {
     const decoded = jwt.verify(token, signature);
     return decoded
 }
